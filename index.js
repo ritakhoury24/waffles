b9d9c556a82b4b8f4e825bb90b45e24254db9bac
rita <rita.elkhoury@icloud.com>
2024-08-20 10:59:12 +0300

    v1

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3031;
+const xlsx=require('xlsx')
+const cors =require('cors')
+ 
+app.use(cors())
+
+
+
+
+let wb2= xlsx.readFile("waffledata.xlsx");
+let ws2 = wb2.Sheets["Sheet1"]
+let dataw = xlsx.utils.sheet_to_json(ws2)
+
+
+
+app.get('/waffles', function (req, res) {
+    res.send(dataw)
+  })
+
+
+ 
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
